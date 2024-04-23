86ea2253b28fc8bb29c89ed86563322364c0032f
Hiro Suyama <hiro.suyama@gmail.com>
2024-04-22 23:51:38 -0300

    Insert links in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
-    return(<div>Navbar</div>)
+    return(
+        <nav>
+            <Link to="/">Website</Link>
+            <ul>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/services">Services</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar;
