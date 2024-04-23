dbaa69ee705b9bc8b00058fcf4f00521de94a27f
Hiro Suyama <hiro.suyama@gmail.com>
2024-04-23 01:03:21 -0300

    Adding Routes and Links and Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import { About, Contact, Home, Services } from './components/pages';
 
-function App() {
+const  App = () => {
   return( 
   <div className="App">
     <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About></About>} />
+      <Route path="/services" element={<Services></Services>} />
+      <Route path="/contact" element={<Contact></Contact>} />
+    </Routes>
   </div>);
 }
 
